3b0731f412a28314e2ccdc3c2082ec87f2bd00e1
Christian Krakau-Louis <christianlouis@gmail.com>
2022-02-25 14:49:18 +0100

    Update initial-docker-setup.sh

--- initial-docker-setup.sh
+++ initial-docker-setup.sh
@@ -9,3 +9,8 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt update
 sudo apt install docker-ce -y
+sudo systemctl status docker
+sudo systemctl enable docker.service
+sudo mkdir -p /usr/local/bin
+sudo curl -SL https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+sudo chmod 755 /usr/local/bin/docker-compose
